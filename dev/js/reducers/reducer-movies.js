27ed003172cbd94af18582aa214b11ac088465e6
Frisco Miranda <friscomiranda@gmail.com>
2016-08-22 15:45:22 -0700

    first commit

---
+++ dev/js/reducers/reducer-movies.js
@@ -0,0 +1,99 @@
+export default function(){
+    return[
+      {
+        id: 1,
+	title : "My Super Ex-Girlfriend",
+	genres : [
+		"Comedy",
+		"Romance",
+		"Sci-Fi"
+	],
+	imdb : {
+	id : "tt0465624",
+		rating : 5.1,
+		votes : 52175
+	}
+},
+{
+  id: 2,
+	title : "Au Pair",
+	genres : [
+		"Comedy",
+		"Family",
+		"Romance"
+	],
+	imdb : {
+		id : "tt0185895",
+		rating: 6,
+		votes : 1482
+	}
+},
+{
+  id: 3,
+	title : "300: Rise of an Empire",
+	genres : [
+		"Action",
+		"Drama",
+		"Fantasy"
+	],
+	imdb : {
+		id : "tt1253863",
+		rating : 6.3,
+		votes : 208366
+	}
+},
+{
+  id: 4,
+	"title" : "An American Crime",
+	"genres" : [
+		"Biography",
+		"Crime",
+		"Drama"
+	],
+	"imdb" : {
+		"id" : "tt0802948",
+		"rating" : 7.4,
+		"votes" : 24278
+	}
+},
+{
+  id: 5,
+	"title" : "CF: Two Little Letters",
+	"genres" : [
+		"Documentary"
+	],
+	"imdb" : {
+		"id" : "tt2075131",
+		"rating" : null,
+		"votes" : null
+	}
+},
+{
+  id: 6,
+	"title" : "CC 2010",
+	"genres" : [
+		"Short",
+		"Adventure",
+		"Sci-Fi"
+	],
+	"imdb" : {
+		"id" : "tt1428007",
+		"rating" : 5.5,
+		"votes" : 10
+	}
+},
+{
+  id: 7,
+	"title" : "EZ Money",
+	"genres" : [
+		"Family",
+		"Comedy"
+	],
+	"imdb" : {
+		"id" : "tt0443494",
+		"rating" : 3.7,
+		"votes" : 111
+	}
+}
+];
+}
---
+++ dev/js/reducers/reducer-users.js
@@ -0,0 +1,29 @@
+export default function(){
+    return[
+      {
+        id: 1,
+        first: "Frisco",
+        last: "Miranda",
+        description: "He's really chill. Looking forward to purchasing a new electric guitar and learning to shred on it. However he is disappointed the forthcoming apple watch 2 will not include built in cellular data. Missed opportunity Apple, missed opportunity.",
+        age: 26,
+        thumbnail: "https://pbs.twimg.com/profile_images/742571909963866112/pTkQf9io.jpg"
+      },
+      {
+        id: 2,
+        first: "Ruby",
+        last: "Miranda",
+        description: "She's aight. Former Sansum employee, and currently doing nursing school.",
+        age: 24,
+        thumbnail: "http://universalwebsites.theknot.com.s3.amazonaws.com/UniversalWebsites/PageMainImage/5516448283579415/3152562/45855416/med/BDP_Engagement_SantaBarbara_FriscoRuby-222.jpg"
+      },
+      {
+        id: 3,
+        first: "Kenta",
+        last: "Ishii",
+        description: "Kenta played water polo in high school for 2 years. After that he decided to focus more on academics, but before long his love of the water lead him back in the form of crew. He rowed for a year and a half before going to college at UCSB. He currently resides in Santa Monica, and wants a dog",
+        age: 25,
+        thumbnail: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/6/005/048/2ce/3b77ba0.jpg"
+      }
+
+    ];
+}
